da654136eaf1ad015d84479d50612e029529eb67
Gustavo Yee <gustavo.yee@gmail.com>
2021-09-30 15:45:35 -0700

    Initial Commit

---
+++ learning-GH/learning_GHApp.swift
@@ -0,0 +1,17 @@
+//
+//  learning_GHApp.swift
+//  learning-GH
+//
+//  Created by Gustavo Yee on 9/30/21.
+//
+
+import SwiftUI
+
+@main
+struct learning_GHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
